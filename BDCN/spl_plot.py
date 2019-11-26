5d0bb5de549022cdaada9a560ba932a71ad1d5fa
KevinVFeng <kevinvfeng@gmail.com>
2019-11-25 23:14:15 -0800

    add BDCN_self_paced_learning

--- BDCN/spl_plot.py
+++ BDCN/spl_plot.py
@@ -1,1 +1,9 @@
 import matplotlib
+import numpy as np
+import pandas as pd
+csv = pd.read_csv("./results/spl2.csv",names=["index","loss","weight","loss_overall"])
+data_size = 20
+for i in range(data_size):
+    mat = csv[csv["index"]==i]
+    print(mat)
+# print(csv)
